36b9f9c1fc727339ae0ffe8306d4dbe7b5803657
nijashansen <nijas.w.hansen@gmail.com>
2019-09-03 22:14:33 +0200

    added owner

---
+++ Core.Entity/Owner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entity
+{
+    class Owner
+    {
+
+        public int id { get; set; }
+
+        public string name { get; set; }
+    }
+}
