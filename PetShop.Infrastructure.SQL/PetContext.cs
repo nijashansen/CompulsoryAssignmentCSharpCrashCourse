15daecde7070eab0a707cd6d88de97b422ef3e15
nijashansen <nijas.w.hansen@gmail.com>
2019-09-11 10:00:58 +0200

    commit the shit

---
+++ PetShop.Infrastructure.SQL/PetContext.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Infrastructure.SQL
+{
+    public class PetContext: DbContext
+    {
+        public DbSet<Pet> pets { get; set; }
+    }
+}
---
+++ PetShop.Infrastructure.SQL/Repositories/PetRepository.cs
@@ -0,0 +1,47 @@
+﻿using Core.DomainServices;
+using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PetShop.Infrastructure.SQL.Repositories
+{
+    class PetRepository : IPetRepository
+    {
+        private PetContext _context;
+
+        public PetRepository(PetContext context)
+        {
+            _context = context;
+        }
+
+        public Pet CreatePet(Pet pet)
+        {
+            var petToCreate = _context.pets.Add(pet).Entity;
+            _context.SaveChanges();
+            return petToCreate;
+        }
+
+        public Pet Delete(int petToDelete)
+        {
+            var toDelete = _context.Remove(new Pet {ID = petToDelete}).Entity;
+            _context.SaveChanges();
+            return toDelete;
+        }
+
+        public Pet ReadPetById(int id)
+        {
+            return _context.pets.FirstOrDefault(prop => prop.ID == id);
+        }
+
+        public IEnumerable<Pet> ReadPets()
+        {
+            return _context.pets.ToList();
+        }
+
+        public Pet UpdatePet(Pet petToBeUpdated)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
